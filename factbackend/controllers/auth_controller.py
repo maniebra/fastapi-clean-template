0395aeb017177627b363fb18db44f87956aa0112
maniebra <mani.ebra@gmail.com>
2025-08-07 10:55:47 +0330

    add dtos module

--- factbackend/controllers/auth_controller.py
+++ factbackend/controllers/auth_controller.py
@@ -1,24 +1,29 @@
+from uuid import UUID
 from fastapi.routing import APIRouter
 
+from factbackend.dtos.requests.auth.register_user_request import RegisterUserRequestDto
 from factbackend.services import auth_service
 
 AuthController = APIRouter()
+
 
 @AuthController.get("/")
 async def all():
     return await auth_service.get_all_users()
 
+
 @AuthController.post("/")
-async def register_user(
-    username: str,
-    password: str, 
-    passwordConfirmation: str,
-    email: str,
-    phone_number: str,
-    first_name: str,
-    last_name: str
-    ):
-    return await auth_service.register_user(username, password, passwordConfirmation, email, phone_number, first_name, last_name)
+async def register_user(request: RegisterUserRequestDto):
+    return await auth_service.register_user(
+        request.username,
+        request.password,
+        request.passwordConfirmation,
+        request.email,
+        request.phone_number,
+        request.first_name,
+        request.last_name,
+    )
+
 
 @AuthController.delete("/{user_id}")
 async def delete_user_by_id(user_id: UUID):
---
+++ factbackend/dtos/requests/auth/register_user_request.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class RegisterUserRequestDto(BaseModel):
+    username: str
+    password: str
+    passwordConfirmation: str
+    email: str
+    phone_number: str
+    first_name: str
+    last_name: str | None = None
+
